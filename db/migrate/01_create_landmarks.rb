5f4ff624d7641f02dc4ab1275b39344f8a5335e3
joanna <github email address>
2017-10-07 12:46:47 -0500

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -2,7 +2,7 @@ class CreateLandmarks < ActiveRecord::Migration
   def change
     create_table :landmarks do |t|
       t.string :name
-      t.integer :figure_id
+      t.integer :figure_id #belongs to figure
       t.integer :year_completed
     end
   end
--- db/migrate/20171003020156_update_figures.rb
+++ db/migrate/20171003020156_update_figures.rb
@@ -1,5 +1,5 @@
 class UpdateFigures < ActiveRecord::Migration
   def change
-    add_column :figures, :title_id, :integer
+    add_column :figures, :title_id, :integer #title begins to figure
   end
 end
